2a693d6e7bc6091c894b905207dae4be084f2df1
genibus <thomas.barreau@free.fr>
2022-01-10 15:12:15 +0100

    💬 Update some texts

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,16 +14,16 @@
                 The <span class="text-bold">Maiar</span> are a class of beings. As an example <span class="text-bold">Sauron</span>  is a Maiar.
               </li>
               <li>
-              Elrond is the half-elven, master of <span class="text-bold">Rivendell</span>.
+                Elrond is the half-elven, Lord of <span class="text-bold">Rivendell</span>.
              </li>
                <li>
-                 <span class="text-bold">Arwen</span>, the elf that got married to <span class="text-bold">Aragon</span> in The Lord of the Rings gave the name to the <span class="text-bold">WASM of Elrond</span>.
+                 <span class="text-bold">Arwen</span>, the elf that got married to <span class="text-bold">Aragon</span> in The Lord of the Rings, gave the name to the <span class="text-bold">WASM of Elrond</span>.
                </li>
               <li>
                <span class="text-bold">The Mandos</span>, judges of the death in the work of Tolkien, helps programmers test their smart contracts, ensuring their immortality on the blockchain.
               </li>
             </ul>
-            <p>To keep this spirit and as fans ourselves, we introduce you this NFT picturing Beniamin Mincu, the CEO of the blockchain, depicted as Elrond the character from the Lord of The Rings.</p>
+            <p>To keep this spirit and as fans ourselves, we introduce you this NFT picturing Beniamin Mincu, the CEO of the blockchain, depicted as Elrond, the character from the Lord of The Rings.</p>
             <p>The <span class="text-bold">full moon</span> is also an important symbol as it seems to be for the Elrond team. Maiar Exchange was released on a full moon day and set records for a new EGLD ATH.<br>As time passes by, we see several moon cycles, the crypto market lunaticly pumps and dumps. However Beniamin and the Elrond team stand working tirelessly to provide us with the best experience. Many thanks to the Elrond team and <span class="text-bold">Beniamin Mincu, the Immortal Ben</span>.</p>
         </div>
       </div>
